3d203736d26ca160d5bf8cb21449b862a3222d55
herrcore <sergei@openanalysis.net>
2021-08-17 18:43:57 -0400

    added failed hash for crc32

--- algorithms/crc32.py
+++ algorithms/crc32.py
@@ -3,8 +3,8 @@ import zlib
 
 DESCRIPTION = "Standard crc32 hash."
 TYPE = 'unsigned_int'
-TEST_1 = 3632233996
-
+# TEST_1 = 3632233996
+TEST_1 = 3632233997
 
 def hash(data):
     return 0xffffffff & zlib.crc32(data)
