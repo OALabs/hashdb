3b2cc1334e1aab2fd0658e86a23107b46e59c136
herrcore <sergei@openanalysis.net>
2021-09-02 22:01:29 -0400

    added blackmatter ransomware api hash

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -5,7 +5,7 @@
 import os
 import glob
 from importlib import import_module
-files = glob.glob(os.path.dirname(__file__)+"/*.py")
+files = glob.glob(os.path.dirname(__file__) + "/*.py")
 __all__ = [os.path.basename(f)[:-3] for f in files if "__init__" not in f]
 modules = {}
 for module_name in __all__:
