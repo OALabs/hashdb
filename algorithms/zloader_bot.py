3ae28ed4d10cf3115fceb9e446cda53073fca2d0
Myrtus 0x0 <myrtus0x0@protonmail.com>
2021-10-22 13:37:03 -0700

    flake8 compatability

--- algorithms/zloader_bot.py
+++ algorithms/zloader_bot.py
@@ -6,6 +6,7 @@ TYPE = 'unsigned_int'
 # Test must match the exact has of the string 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 TEST_1 = 3694482793
 
+
 def hash(data):
     generated_hash = 0
     for i in range(len(data)):
@@ -18,6 +19,5 @@ def hash(data):
             generated_hash = (v18 & ~v10) | (v10 & v11)
         else:
             generated_hash = v17
-    
-    return generated_hash
 
+    return generated_hash
