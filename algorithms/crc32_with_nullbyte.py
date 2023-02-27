616b543a8e01dcf267b5cbdc3ed2d70de668033b
ThisIsLibra <info@maxkersten.nl>
2023-02-27 18:59:45 +0100

    Added 'crc32_with_nullbyte.py' which is used in PlugX samples

---
+++ algorithms/crc32_with_nullbyte.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+#Copyright by Max 'Libra' Kersten <https://maxkersten.nl>
+
+import zlib
+ 
+DESCRIPTION = "The CRC32 hash of the given string with the terminating null byte added to it. This algorithm is used in PlugX samples."
+TYPE = 'unsigned_int'
+TEST_1 = 3133185683
+ 
+ 
+def hash(data):
+  data = data + b'\x00'
+  return 0xffffffff & zlib.crc32(data)
