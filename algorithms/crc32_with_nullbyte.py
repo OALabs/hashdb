1981c71bcea0e53a57ba39fec949771bb055dc25
ThisIsLibra <info@maxkersten.nl>
2023-02-27 19:03:14 +0100

    Fixed space usage from 2 into 4

--- algorithms/crc32_with_nullbyte.py
+++ algorithms/crc32_with_nullbyte.py
@@ -3,12 +3,11 @@
 #Copyright by Max 'Libra' Kersten <https://maxkersten.nl>
 
 import zlib
- 
+
 DESCRIPTION = "The CRC32 hash of the given string with the terminating null byte added to it. This algorithm is used in PlugX samples."
 TYPE = 'unsigned_int'
 TEST_1 = 3133185683
- 
- 
+
 def hash(data):
-  data = data + b'\x00'
-  return 0xffffffff & zlib.crc32(data)
+    data = data + b'\x00'
+    return 0xffffffff & zlib.crc32(data)
