79d724b4fa5359f08e95430083898f347c2717a0
jershmagersh <jmag@pwnage.io>
2023-10-20 11:12:03 -0400

    Adding algorithm for adding 65599 and multiplying the result.

---
+++ algorithms/add_65599.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+DESCRIPTION = "ADD 65599 and MULTIPLY"
+TYPE = 'unsigned_int'
+TEST_1 = 2480409887
+
+def hash(data):
+    result = 0
+    for c in data:
+        tmp = ord(c) + 32
+        if(((ord(c) - ord('A')) & 0xFFFF) > 26):
+            tmp = ord(c)
+        result = (tmp + 0x1003F * result) & 0xFFFFFFFF
+    
+    return result
