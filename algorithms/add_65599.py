434b83725088cc0d99bb4e9ddb1669b13a79b089
jershmagersh <jmag@pwnage.io>
2023-10-20 11:25:15 -0400

    Fixing for byte strings as input.

--- algorithms/add_65599.py
+++ algorithms/add_65599.py
@@ -6,9 +6,9 @@ TEST_1 = 2480409887
 def hash(data):
     result = 0
     for c in data:
-        tmp = ord(c) + 32
-        if(((ord(c) - ord('A')) & 0xFFFF) > 26):
-            tmp = ord(c)
+        tmp = c + 32
+        if(((c - ord('A')) & 0xFFFF) > 26):
+            tmp = c
         result = (tmp + 0x1003F * result) & 0xFFFFFFFF
     
     return result
