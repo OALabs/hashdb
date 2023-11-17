caf4ade8379a29dbaf88dc6a78345311dcc124f6
Still Hsu <dev@stillu.cc>
2023-11-17 16:19:10 +0800

    Add initial MileRAT hashing algo
    Signed-off-by: Still Hsu <dev@stillu.cc>

---
+++ algorithms/milerat.py
@@ -0,0 +1,11 @@
+# Created by Still Hsu <still@teamt5.org>
+
+DESCRIPTION = "Hashing algorithm used by MileRAT."
+TYPE = 'unsigned_int'
+TEST_1 = 150583839
+
+def hash(data):
+    esi = 0
+    for c in data:
+        esi = (ord(c) + 0x83 * esi) & 0xffffffff
+    return (esi & 0x7fffffff)
