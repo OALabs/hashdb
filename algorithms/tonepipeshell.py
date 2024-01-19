f5e006c06227729845101d548c4067f814d2f6f3
Still Hsu <dev@stillu.cc>
2024-01-19 13:55:32 +0800

    Add TOnePipeShell hashing algo

---
+++ algorithms/tonepipeshell.py
@@ -0,0 +1,11 @@
+# Created by Still Hsu <still@teamt5.org>
+
+DESCRIPTION = "TOnePipeShell hash with seed 0xC85E31"
+TYPE = 'unsigned_int'
+TEST_1 = 3454880715
+
+def hash(data):
+    out_hash = 0
+    for c in data:
+        out_hash = (c + 0xC85E31 * out_hash) & 0xffffffff
+    return out_hash
